5d6b6af255e6cc2680c9484d4516b4c2e2f50ba2
Dirk Mueller <dirkmuller8@outlook.de>
2023-06-08 08:16:40 +0200

    Add project files.

---
+++ QuadEqWithInputVal/CoeffValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadEqWithInputVal
+{
+    public class CoeffValidator : AbstractValidator<Coefficients>
+    {
+        float maxSize = float.MaxValue;
+        public CoeffValidator()
+        {
+            RuleFor(x => x.a)
+                .NotEqual(0)
+                .WithMessage("Input for a cannot be zero: no quadratic equation")
+                .LessThan(maxSize)
+                .WithMessage("Input for a too big");
+
+            RuleFor(x => x.b)
+                .NotEmpty()
+                .WithMessage("Input for b cannot be empty")
+                .LessThan(maxSize)
+                .WithMessage("Input for b too big");
+
+            RuleFor(x => x.c)
+                .NotEmpty()
+                .WithMessage("Input for c cannot be empty")
+                .LessThan(maxSize)
+                .WithMessage("Input for c too big");
+        }
+    }
+}
---
+++ QuadEqWithInputVal/Program.cs
@@ -0,0 +1,135 @@
+﻿global using FluentValidation;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace QuadEqWithInputVal
+{
+    public class Coefficients
+    {
+        public float a { get; set; }
+        public float b { get; set; }
+        public float c { get; set; }
+        public Coefficients(float a, float b, float c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+
+    internal class Program
+    {
+        public static void DisplayContents(ICollection keyCollection, ICollection valueCollection, int dictionarySize)
+        {
+            String[] myKeys = new String[dictionarySize];
+            float[] myValues = new float[dictionarySize];
+            keyCollection.CopyTo(myKeys, 0);
+            valueCollection.CopyTo(myValues, 0);
+
+            // Displays the contents of the OrderedDictionary
+            Console.WriteLine("   INDEX KEY                       VALUE");
+            for (int i = 0; i < dictionarySize; i++)
+            {
+                Console.WriteLine("   {0,-5} {1,-25} {2}",
+                    i, myKeys[i], myValues[i]);
+            }
+            Console.WriteLine();
+        }
+        public static string DisplayKey(ICollection keyCollection, int dictionarySize, int i)
+        {
+            String[] myKeys = new String[dictionarySize];
+            keyCollection.CopyTo(myKeys, 0);
+
+            return myKeys[i];
+
+        }
+        public static float DisplayValue(ICollection valueCollection, int dictionarySize, int i)
+        {
+            float[] myValues = new float[dictionarySize];
+            valueCollection.CopyTo(myValues, 0);
+
+            return myValues[i];
+
+        }
+        static void Main(string[] args)
+        {
+            string str;
+            bool resultParse;
+
+            float a, b, c;
+            float temp;
+            float[] coeffM = new float[3];
+            OrderedDictionary myOrdDict = new OrderedDictionary();
+            myOrdDict.Add("a", 0.0F);
+            myOrdDict.Add("b", 0.0F);
+            myOrdDict.Add("c", 0.0F);
+            ICollection keyCollection = myOrdDict.Keys;
+            ICollection valueCollection = myOrdDict.Values;
+
+            while (true)
+            {
+                bool check = false;
+                for (int i = 0; i < 3; i++)
+                {
+                    while (true)
+                    {
+                        while (true)
+                        {
+                            Console.Write($"Give {DisplayKey(keyCollection, myOrdDict.Count, i)}: ");
+                            str = Console.ReadLine();
+                            if (str.Contains('.'))
+                            {
+                                Console.WriteLine("Please us a comma, instead of .!");
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+
+                        resultParse = float.TryParse(str, out temp);
+                        if (!resultParse)
+                        {
+                            Console.WriteLine($"Coefficient {myOrdDict[DisplayKey(keyCollection, myOrdDict.Count, i)]} is not a double!");
+                        }
+                        else
+                        {
+                            myOrdDict[DisplayKey(keyCollection, myOrdDict.Count, i)] = temp;
+                            DisplayContents(keyCollection, valueCollection, myOrdDict.Count);
+                            break;
+                        }
+                    }
+
+                }
+                Coefficients coefficients = new Coefficients(DisplayValue(valueCollection, myOrdDict.Count, 0), DisplayValue(valueCollection, myOrdDict.Count, 1), DisplayValue(valueCollection, myOrdDict.Count, 2));
+                CoeffValidator validator = new CoeffValidator();
+                var result = validator.Validate(coefficients);
+
+                if (!result.IsValid)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        Console.WriteLine(error.ErrorMessage);
+                        check = true;
+                    }
+                }
+                if (coefficients.a == 0.0)
+                {
+                    Console.WriteLine($"Coefficient {myOrdDict[DisplayKey(keyCollection, myOrdDict.Count, 0)]} cannot be zero!");
+                    check = true;
+
+                }
+                if (!check)
+                {
+                    break;
+                }
+                for (int i = 0; i < 2; i++)
+                {
+                    myOrdDict[DisplayKey(keyCollection, myOrdDict.Count, i)] = 0;
+                }
+
+            }
+            Console.WriteLine("Bin am Ende angekommen");
+        }
+    }
+}
